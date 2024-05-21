a06de9fa1e02f619ae2e7bde79f4c091aea48bc9
Stijn Vet <stijn.vet@openvalue.nl>
2024-05-21 19:22:18 +0200

    Deploy to EB

--- src/main/kotlin/nl/openvalue/demo/DemoApplication.kt
+++ src/main/kotlin/nl/openvalue/demo/DemoApplication.kt
@@ -2,9 +2,19 @@ package nl.openvalue.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class DemoApplication
+@RestController
+class DemoApplication {
+
+	@GetMapping("/hello", "hello")
+	fun greet(): ResponseEntity<String> {
+		return ResponseEntity.ok("Hello, World!")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
